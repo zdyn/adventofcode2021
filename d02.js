5a477866f29ea9d9dea65aafafd53062a5d97ac0
Jing Brian Zhou <bzthemusician@gmail.com>
2021-12-01 21:31:26 -0800

    Remove unnecessary aim var

--- d02.js
+++ d02.js
@@ -3,20 +3,18 @@ const cmds = input.split("\n").map((l) => {
   const p = l.split(" ");
   return {cmd: p[0], n: Number(p[1])};
 });
-let pos = 0, dep1 = 0, dep2 = 0, aim = 0;
+let pos = 0, dep1 = 0, dep2 = 0;
 for (let {cmd, n} of cmds) {
   switch (cmd) {
     case "forward":
       pos += n;
-      dep2 += aim * n;
+      dep2 += dep1 * n;
       break;
     case "down":
       dep1 += n;
-      aim += n;
       break
     case "up":
       dep1 -= n;
-      aim -= n;
       break;
   }
 }
