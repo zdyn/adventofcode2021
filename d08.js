8fb33043abd68408149158f440048c207edc9896
Jing Brian Zhou <bzthemusician@gmail.com>
2021-12-07 23:07:58 -0800

    Use Katherine's solution

--- d08.js
+++ d08.js
@@ -9,67 +9,41 @@ let sum = entries
   .map((e) => e[1].filter((s) => [2, 3, 4, 7].includes(s.length)).length)
   .reduce((agg, n) => agg + n, 0)
 console.log(sum);
-const segments = "abcdefg".split("");
-const nums = {
-  "abcefg": 0,
-  "cf": 1,
-  "acdeg": 2,
-  "acdfg": 3,
-  "bcdf": 4,
-  "abdfg": 5,
-  "abdefg": 6,
-  "acf": 7,
-  "abcdefg": 8,
-  "abcdfg": 9,
-};
 Set.prototype.intersection = function(s) {
-  [...this.values()]
-    .filter((n) => !s.has(n))
-    .forEach((n) => this.delete(n));
-  return this;
-};
-Set.prototype.difference = function(s) {
-  [...s.values()].forEach((n) => this.delete(n));
-  return this;
+  return new Set([...this.values()].filter((n) => s.has(n)));
 };
 sum = 0;
+// All credit to Katherine Threlkeld.
 for (let [signals, output] of entries) {
-  const possible = {};
-  for (let s of segments) {
-    possible[s] = new Set(segments);
-  }
-  signals.sort((a, b) => a.length - b.length);
+  let one = null, four = null, seven = null;
   for (let signal of signals) {
-    const set = new Set(signal.split(""));
-    switch (signal.length) {
-      case 2:
-        "cf".split("").forEach((c) => possible[c] = set);
-        break;
-      case 3:
-        "acf".split("").forEach((c) => possible[c].intersection(set));
-        possible["a"].difference(possible["c"]);
-        break;
-      case 4:
-        "bcdf".split("").forEach((c) => possible[c].intersection(set));
-        "bd".split("").forEach((c) => possible[c].difference(possible["c"]));
-        break;
-      case 6:
-        const diff = new Set(segments).difference(set);
-        if (possible["c"].has([...diff.values()][0])) possible["c"] = diff;
-        else if (possible["d"].has([...diff.values()][0])) possible["d"] = diff;
-        else possible["e"] = diff;
-        break;
-    }
+    let s = new Set(signal.split(""));
+    if (signal.length === 2) one = s;
+    else if (signal.length === 3) seven = s;
+    else if (signal.length === 4) four = s;
   }
-  possible["b"].difference(possible["d"]);
-  possible["f"].difference(possible["c"]);
-  "abcdef".split("").forEach((c) => possible["g"].difference(possible[c]));
-  const reverse = {};
-  for (let k in possible) {
-    reverse[[...possible[k].values()][0]] = k;
-  }
-  sum += Number(
-    output.map((o) => nums[o.split("").map((c) => reverse[c]).sort().join("")]).join("")
-  );
+  let n = output
+    .map((o) => {
+      o = new Set(o.split(""));
+      switch (o.size) {
+        case 2: return 1;
+        case 3: return 7;
+        case 4: return 4;
+        case 5:
+          if (o.intersection(one).size === 2) return 3;
+          else if (o.intersection(four).size === 2) return 2;
+          else return 5;
+        case 6:
+          if (o.intersection(seven).size === 3) {
+            if (o.intersection(four).size === 4) return 9;
+            else return 0;
+          } else {
+            return 6;
+          }
+        case 7: return 8;
+      }
+    })
+    .join("");
+  sum += Number(n);
 }
 console.log(sum);
