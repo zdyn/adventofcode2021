852ae20ca02b67afca7fc565b365ae7f770a1631
Jing Brian Zhou <bzthemusician@gmail.com>
2021-12-15 01:27:11 -0800

    Speed up and add comments

--- d15.js
+++ d15.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S deno run --allow-read
 
 const input = await Deno.readTextFile("d15.txt");
-const nums = [9, 1, 2, 3, 4, 5, 6, 7, 8];
+// Returns an array of [row, column] for cardinally adjacent neighbors.
 const adj = (i, j) => {
   return [
     [i + 1, j],
@@ -11,30 +11,51 @@ const adj = (i, j) => {
   ];
 };
 const cave = input.trim().split(/\n/g).map((l) => l.split("").map(Number));
+// Used for extending the grid.
+const nums = [9, 1, 2, 3, 4, 5, 6, 7, 8];
+// The extended cave.
 const cave2 = [...Array(cave.length * 5).keys()].map((i) => {
   return [...Array(cave.length * 5).keys()].map((j) => {
+    // diff is the "distance" of the duplicated grid from the original.
     const diff = ~~(i / cave.length) + ~~(j / cave.length);
     return nums[(cave[i % cave.length][j % cave.length] + diff) % 9];
   });
 });
 const minSum = (cave) => {
+  // Keeps track of changed cells. Using a set would require transforming the
+  // tuples to a valid key, which is much slower.
+  let changed = [];
+  // agg will eventually represent the minimum distances from 0,0.
   const agg = cave.map((row) => row.map(() => Infinity));
   agg[0][0] = cave[0][0];
-  while (true) {
-    let changes = 0;
-    for (let i = 0; i < cave.length; i++) {
-      for (let j = 0; j < cave.length; j++) {
-        for (let [r, c] of adj(i, j)) {
-          if (
-            r < 0 || c < 0 || r >= cave.length || c >= cave.length ||
-            agg[r][c] === Infinity || agg[i][j] <= agg[r][c] + cave[i][j]
-          ) continue;
-          changes++;
-          agg[i][j] = agg[r][c] + cave[i][j];
+  // Do a first pass using just down and right "movements".
+  for (let i = 0; i < cave.length; i++) {
+    for (let j = 0; j < cave.length; j++) {
+      changed.push([i, j]);
+      for (let [r, c] of adj(i, j)) {
+        // Ignore out-of-bounds neighbors.
+        if (r < 0 || c < 0 || r >= cave.length || c >= cave.length) continue;
+        // For the first pass, ignore down and right neighbors.
+        if (r > i || c > j) continue;
+        agg[i][j] = Math.min(agg[i][j], agg[r][c] + cave[i][j]);
+      }
+    }
+  }
+  // Repeat passes for changed cells until cells stop changing.
+  while (changed.length > 0) {
+    const next = [];
+    // For each changed cell, check if there is a shorter path from it to a neighbor.
+    for (let [i, j] of changed) {
+      for (let [r, c] of adj(i, j)) {
+        // Ignore out-of-bounds neighbors.
+        if (r < 0 || c < 0 || r >= cave.length || c >= cave.length) continue;
+        if (agg[i][j] + cave[r][c] < agg[r][c]) {
+          agg[r][c] = agg[i][j] + cave[r][c];
+          next.push([r, c]);
         }
       }
     }
-    if (changes === 0) break;
+    changed = next;
   }
   return agg[agg.length - 1][agg.length - 1];
 };
