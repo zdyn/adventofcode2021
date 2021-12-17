5663523e8a3775671cd6dafa95c85d471183fbb3
Jing Brian Zhou <bzthemusician@gmail.com>
2021-12-16 21:42:05 -0800

    Classify

--- d16.js
+++ d16.js
@@ -4,55 +4,65 @@ const input = await Deno.readTextFile("d16.txt");
 const bits = input
   .trim()
   .split("")
-  .map((c) => ("0000" + parseInt(c, 16).toString(2)).slice(-4))
+  .map((c) => parseInt(c, 16).toString(2).padStart(4, "0"))
   .join("");
 
-const parse = () => {
-  versionSum += parseInt(bits.slice(i, i + 3), 2);
-  const type = parseInt(bits.slice(i + 3, i + 6), 2);
-  i += 6;
+class Packet {
+  constructor(bits) {
+    this.bits = bits;
+    this.subPackets = [];
+    this.valueBits = "";
 
-  // Logical.
-  if (type === 4) {
-    let groups = [];
-    while (true) {
-      groups.push(bits.slice(i + 1, i + 5));
-      i += 5;
-      if (bits[i - 5] === "0") break;
+    let i = 0;
+    this.version = parseInt(bits.slice(i, i + 3), 2);
+    this.type = parseInt(bits.slice(i + 3, i + 6), 2);
+    i += 6;
+    if (this.type === 4) {
+      while (true) {
+        this.valueBits += bits.slice(i + 1, i + 5);
+        i += 5;
+        if (bits[i - 5] === "0") break;
+      }
+    } else {
+      if (bits[i] === "0") {
+        const target = i + 16 + parseInt(bits.slice(i + 1, i + 16), 2);
+        i += 16;
+        while (i < target) {
+          const p = new Packet(bits.slice(i));
+          i += p.bits.length;
+          this.subPackets.push(p);
+        }
+      } else {
+        const remaining = parseInt(bits.slice(i + 1, i + 12), 2);
+        i += 12;
+        while (this.subPackets.length < remaining) {
+          const p = new Packet(bits.slice(i));
+          i += p.bits.length;
+          this.subPackets.push(p);
+        }
+      }
     }
-    return parseInt(groups.join(""), 2);
+    this.bits = this.bits.slice(0, i);
   }
 
-  // Operator.
-  const vals = [];
-  if (bits[i] === "0") {
-    const target = i + 16 + parseInt(bits.slice(i + 1, i + 16), 2);
-    i += 16;
-    while (i < target) {
-      vals.push(parse());
-    }
-  } else {
-    let remaining = parseInt(bits.slice(i + 1, i + 12), 2);
-    i += 12;
-    while (remaining > 0) {
-      vals.push(parse());
-      remaining--;
-    }
+  versionSum() {
+    return this.version + this.subPackets.reduce((agg, p) => agg + p.versionSum(), 0);
   }
-  switch (type) {
-    case 0: return vals.reduce((agg, n) => agg + n, 0);
-    case 1: return vals.reduce((agg, n) => agg * n, 1);
-    case 2: return Math.min.apply(null, vals);
-    case 3: return Math.max.apply(null, vals);
-    case 5: return vals[0] > vals[1] ? 1 : 0;
-    case 6: return vals[0] < vals[1] ? 1 : 0;
-    case 7: return vals[0] === vals[1] ? 1 : 0;
-  }
-};
 
-// Index pointer into bits.
-let i = 0;
-let versionSum = 0;
-const outer = parse();
-console.log(versionSum);
-console.log(outer);
+  value() {
+    switch (this.type) {
+      case 0: return this.subPackets.reduce((agg, n) => agg + n.value(), 0);
+      case 1: return this.subPackets.reduce((agg, n) => agg * n.value(), 1);
+      case 2: return Math.min.apply(null, this.subPackets.map((n) => n.value()));
+      case 3: return Math.max.apply(null, this.subPackets.map((n) => n.value()));
+      case 4: return parseInt(this.valueBits, 2);
+      case 5: return this.subPackets[0].value() > this.subPackets[1].value() ? 1 : 0;
+      case 6: return this.subPackets[0].value() < this.subPackets[1].value() ? 1 : 0;
+      case 7: return this.subPackets[0].value() === this.subPackets[1].value() ? 1 : 0;
+    }
+  }
+}
+
+const p = new Packet(bits);
+console.log(p.versionSum());
+console.log(p.value());
